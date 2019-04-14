c7064497242cdbad1a9aac0d870c8ea8978aafc3
nguyenhongphat0 <nguyenhongphat28121998@gmail.com>
2019-04-14 14:00:42 +0700

    register page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
-
+import {NgbDate, NgbCalendar} from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit, OnDestroy {
+  dob: NgbDate;
+
   constructor(private router: Router) {}
 
   ngOnInit() {
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbDate } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  dob: NgbDate;
+  done: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  register() {
+    this.done = true;
+  }
 }
