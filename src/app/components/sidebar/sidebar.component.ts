4a0ab9cc9cda0ee937452b1634730cae39d8cae7
nguyenhongphat0 <nguyenhongphat28121998@gmail.com>
2019-04-23 11:43:16 +0700

    change icons and cleanup examples

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -11,15 +11,6 @@ export const ROUTES: RouteInfo[] = [
     { path: '/club-members', title: 'Club Members',  icon:'ni-bullet-list-67 text-red', class: '' },
     { path: '/member-connect', title: 'Member Connect',  icon:'ni-planet text-blue', class: '' }
 ];
-export const EXAMPLEROUTES: RouteInfo[] = [
-    { path: '/dashboard', title: 'Dashboard',  icon: 'ni-tv-2 text-primary', class: '' },
-    { path: '/icons', title: 'Icons',  icon:'ni-planet text-blue', class: '' },
-    { path: '/maps', title: 'Maps',  icon:'ni-pin-3 text-orange', class: '' },
-    { path: '/user-profile', title: 'User profile',  icon:'ni-single-02 text-yellow', class: '' },
-    { path: '/tables', title: 'Tables',  icon:'ni-bullet-list-67 text-red', class: '' },
-    { path: '/login', title: 'Login',  icon:'ni-key-25 text-info', class: '' },
-    { path: '/register', title: 'Register',  icon:'ni-circle-08 text-pink', class: '' }
-];
 
 @Component({
   selector: 'app-sidebar',
@@ -36,7 +27,6 @@ export class SidebarComponent implements OnInit {
 
   ngOnInit() {
     this.menuItems = ROUTES;
-    this.exampleMenuItems = EXAMPLEROUTES;
     this.router.events.subscribe((event) => {
       this.isCollapsed = true;
    });
--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -1,19 +1,9 @@
 import { Routes } from '@angular/router';
 
-import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
-import { IconsComponent } from '../../pages/icons/icons.component';
-import { MapsComponent } from '../../pages/maps/maps.component';
-import { UserProfileComponent } from '../../pages/user-profile/user-profile.component';
-import { TablesComponent } from '../../pages/tables/tables.component';
 import { ClubMembersComponent } from 'src/app/pages/club-members/club-members.component';
 import { MemberConnectComponent } from 'src/app/pages/member-connect/member-connect.component';
 
 export const AdminLayoutRoutes: Routes = [
     { path: 'club-members',   component: ClubMembersComponent },
-    { path: 'member-connect',   component: MemberConnectComponent },
-    { path: 'dashboard',      component: DashboardComponent },
-    { path: 'user-profile',   component: UserProfileComponent },
-    { path: 'tables',         component: TablesComponent },
-    { path: 'icons',          component: IconsComponent },
-    { path: 'maps',           component: MapsComponent }
+    { path: 'member-connect',   component: MemberConnectComponent }
 ];
