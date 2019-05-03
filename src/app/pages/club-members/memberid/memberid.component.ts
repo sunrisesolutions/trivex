5a99d649136d93e80e90aed6c43fefb5f8324eab
nguyenhongphat0 <hongphat.js@gmail.com>
2019-05-03 22:56:41 +0700

    post connection api

--- src/app/pages/club-members/memberid/memberid.component.ts
+++ src/app/pages/club-members/memberid/memberid.component.ts
@@ -3,6 +3,8 @@ import { PostService } from "src/app/services/post.service";
 import * as jwt_decode from "jwt-decode";
 import { getRootComponents } from "@angular/core/src/render3/discovery_utils";
 import { HttpParams } from "@angular/common/http";
+import { OrganisationService } from "../../../services/organisation.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "info",
@@ -10,15 +12,21 @@ import { HttpParams } from "@angular/common/http";
   styleUrls: ["./memberid.component.scss"]
 })
 export class MemberidComponent implements OnInit {
-  constructor(private service: PostService) {}
+  constructor(
+    private service: PostService,
+    private orgService: OrganisationService,
+    private router: Router
+  ) {}
   id;
-  members;
+  member;
+  loading = false;
+  error = '';
+
   ngOnInit() {
-    let id;
     this.service.getDataAPI().subscribe(response => {
       let get = response.json()["hydra:member"]["0"]["@id"];
-      id = get;
-      this.getRootID(id);
+      this.id = get;
+      this.getRootID(this.id);
     });
   }
 
@@ -26,11 +34,18 @@ export class MemberidComponent implements OnInit {
     this.service.getRootID(id).subscribe(res => {
       console.log(res.json().personData);
       let getDATA = res.json().personData;
-      this.members = [getDATA];
+      this.member = getDATA;
     });
   }
   
   send() {
-    alert('Sent!');
+    this.loading = true;
+    this.orgService.connectToMember(this.id).subscribe(res => {
+      this.loading = false;
+      this.router.navigate([`/club-members/${this.id}/connect`]);
+    }, err => {
+      this.error = err.error['hydra:description'];
+      this.loading = false;
+    });
   }
 }
