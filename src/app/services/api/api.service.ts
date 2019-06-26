a4302b0223b5f34c9b44795853490eac9609954d
truong <truong@localhost.localdomain>
2019-06-27 01:00:39 +0700

    FIXED_HEADER

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -14,14 +14,14 @@ export class ApiService {
 
   constructor(private http: HttpClient) { }
 
-  httpOptions = {
-    headers: new HttpHeaders({
-      "accept": "application/ld+json",
-      "Authorization": `Bearer ${localStorage.getItem("token")}`
-    })
-  };
-  userInfo(id) {
 
-    return this.http.get(`${userAPI}/individual_members/${id}`, this.httpOptions);
+  userInfo(id) {
+    httpOptions = {
+      headers: new HttpHeaders({
+        "accept": "application/ld+json",
+        "Authorization": `Bearer ${localStorage.getItem("token")}`
+      })
+    };
+    return this.http.get(`${userAPI}/individual_members/${id}`, httpOptions);
   }
 }
